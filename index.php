bba8c5158fc6ad0f5dd14da139d0bbd7c28f4016
robinNcode <msmrobin518@gmail.com>
2023-02-22 23:53:30 +0600

    Slide content added

--- index.php
+++ index.php
@@ -1,9 +1,151 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-    <title>My Page</title>
+    <title>Animated Slider</title>
 </head>
+
 <body>
-    
+    <!-- Container Start -->
+    <div class="container">
+        <!-- Silde 1 Start -->
+        <div class="slide-container">
+            <div class="slide">
+                <div class="content">
+                    <h3>PROJECT THESIS MANAGEMENT
+                        SYSTEM</h3>
+                    <p>
+                        The Project-Thesis Management
+                        System is a cloud-based solution that
+                        helps manage academic projects and
+                        theses. It provides a central platform
+                        for students and faculty to collaborate,
+                        communicate, and track the progress of
+                        their projects and theses.
+                    </p>
+                </div>
+                <div class="image">
+                    <img src="./img/casual-life-3d-graduates-hat-and-books.png" alt="">
+                </div>
+            </div>
+        </div>
+        <!-- Silde 1 End -->
+        <!-- Silde 2 Start -->
+        <div class="slide-container">
+            <div class="slide">
+                <div class="content">
+                    <h3>ASSIGN PROJECT THESIS TO GROUP</h3>
+                    <p>
+                        Creating a project thesis specific
+                        to a group ensures a collective
+                        effort towards achieving the
+                        research objectives. It allows
+                        each member to contribute their
+                        expertise and collaborate in a
+                        structured manner. This results
+                        in a comprehensive, well-
+                        researched thesis that reflects
+                        the group's capabilities and
+                        expertise.
+                    </p>
+                </div>
+                <div class="image">
+                    <img src="./img/discussing-team-project.png" alt="">
+                </div>
+            </div>
+        </div>
+        <!-- Silde 2 End -->
+
+        <!-- Silde 3 Start -->
+        <div class="slide-container">
+            <div class="slide">
+                <div class="content">
+                    <h3>PROJECT MODULE SECTION</h3>
+                    <p>
+                        It helps in dividing
+                        responsibilities, preventing
+                        overlap, and ensuring that each
+                        group member utilizes their skills
+                        and knowledge to the fullest.
+                        This process also enhances
+                        accountability and improves the
+                        overall quality of work delivered.
+                    </p>
+                </div>
+                <div class="image">
+                    <img src="./img/bouncy-project-management-workflow.png" alt="">
+                </div>
+            </div>
+        </div>
+        <!-- Silde 3 End -->
+        <!-- Silde 4 Start -->
+        <div class="slide-container">
+            <div class="slide">
+                <div class="content">
+                    <h3>TRACE PROJECT THESIS PROGRESS</h3>
+                    <p>
+                        A supervisor can easily monitor
+                        the activity and progress of their
+                        assigned groups. It helps in
+                        identifying areas that need
+                        improvement and ensures
+                        timely completion of tasks.
+                    </p>
+                </div>
+                <div class="image">
+                    <img src="./img/finish-project-work-faster.png" alt="">
+                </div>
+            </div>
+        </div>
+        <!-- Silde 4 End -->
+
+        <!-- Silde 5 Start -->
+        <div class="slide-container">
+            <div class="slide">
+                <div class="content">
+                    <h3>SCHEDULE ASSIGN AND
+                        ATTENDANCE</h3>
+                    <p>
+                        Scheduling notifications via email
+                        and storing attendance helps in
+                        keeping track of group meetings and
+                        deadlines. It ensures that all members
+                        are informed and accountable, leading
+                        to better project management and
+                        timely completion.
+                    </p>
+                </div>
+                <div class="image">
+                    <img src="./img/silky-time-management.png" alt="">
+                </div>
+            </div>
+        </div>
+        <!-- Silde 5 End -->
+        <!-- Silde 6 Start -->
+        <div class="slide-container">
+            <div class="slide">
+                <div class="content">
+                    <h3>PUBLISHED PROJECT
+                        THESIS</h3>
+                    <p>
+                        Publishing project theses online
+                        enables easy access and analysis by
+                        anyone interested in the topic.
+                        Searching allows for targeted results,
+                        facilitating research and development.
+                        This encourages collaboration and
+                        sharing of knowledge for the benefit of
+                        all.
+                    </p>
+                </div>
+                <div class="image">
+                    <img src="./img/publish_project.png" alt="">
+                </div>
+            </div>
+        </div>
+        <!-- Silde 6 End -->
+    </div>
+
 </body>
+
 </html>
